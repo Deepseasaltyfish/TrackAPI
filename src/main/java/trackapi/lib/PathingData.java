0e240de9488b412f76b22019399547ed4a8f5b65
Deepseasaltyfish <h20040215@163.com>
2026-03-03 15:34:42 +0800

    add isUpdate

--- src/main/java/trackapi/lib/PathingData.java
+++ src/main/java/trackapi/lib/PathingData.java
@@ -9,11 +9,13 @@ public class PathingData {
     private Vec3d vanillaPos;
     private double roll;
     private double deltaMovement;
+    private boolean updated;
 
     public PathingData(Vec3d vanillaPos, double roll, double deltaMovement) {
         this.vanillaPos = vanillaPos;
         this.roll = roll;
         this.deltaMovement = deltaMovement;
+        this.updated = false;
     }
 
     public Vec3d getVanillaPos() {
@@ -21,6 +23,8 @@ public class PathingData {
     }
 
     public void setVanillaPos(Vec3d pos) {
+        if(this.vanillaPos.equals(pos)) return;
+        updated = true;
         this.vanillaPos = pos;
         this.deltaMovement += this.vanillaPos.distanceTo(pos);
     }
@@ -30,10 +34,16 @@ public class PathingData {
     }
 
     public void setRoll(double roll) {
+        if(Double.compare(this.roll, roll) == 0) return;
+        updated = true;
         this.roll = roll;
     }
 
     public double getDeltaMovement() {
         return deltaMovement;
+    }
+
+    public boolean isUpdated() {
+        return updated;
     }
 }
